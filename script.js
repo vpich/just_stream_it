bedb85188e151322e9b2da6ef3e9459065599104
Virginie PICH <red_iv@hotmail.fr>
2022-10-17 16:38:21 +0200

    Création du script.js + début de structure en commentaires

---
+++ script.js
@@ -0,0 +1,10 @@
+// Récupération des miniatures pour film
+
+const best_movie_miniature = document.getElementById("")
+const category_1_miniature = document.querySelector("")
+
+// Gestion slides
+
+// Gestion bouton play
+
+// Gestion fenêtre modale
