8a86df012c392df13aa95f10c40120d440cee2e5
Virginie PICH <red_iv@hotmail.fr>
2022-10-24 16:55:56 +0200

    Améliorations de mise en page

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
+console.log("coucou")
+
 // Récupération des miniatures pour film
 
-const best_movie_miniature = document.getElementById("")
-const category_1_miniature = document.querySelector("")
+//const best_movie_miniature = document.getElementById("")
+//const category_1_miniature = document.querySelector("")
 
 // Gestion slides
 
 // Gestion bouton play
 
 // Gestion fenêtre modale
+
+const bouton = document.getElementById("toto")
+const para = document.getElementById("tata")
+console.log(para)
+
+bouton.addEventListener("click", function(){
+    para.innerHTML = "Virginie le retour"
+    console.log("click")
+})
+
+fetch("https://api.publicapis.org/entries").then(function(res){
+    console.log(res.json());
+})
