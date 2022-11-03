b34fdac4bc0a827eb8200781928abd13f33741d2
Virginie PICH <red_iv@hotmail.fr>
2022-11-03 17:37:04 +0100

    Création des fonctions et boucles nécessaires à l'affichage des bonnes images de film dans 3 catégories de films

--- script.js
+++ script.js
@@ -2,23 +2,40 @@
 
 //const best_movie_miniature = document.getElementById("")
 //const category_1_miniature = document.querySelector("")
+const BASE_URL = "http://localhost:8000/api/v1/titles/"
+const genre_filter = "genre="
+const rating_filter = "imdb_score="
+const SORT_BY_RATING = "sort_by=-imdb_score"
 
 //http://localhost:8000/api/v1/titles/?genre=animation
 //http://localhost:8000/api/v1/titles/?imdb_score=&genre=animation
 
 //A faire
 //  1 - Gestion slides
-//  2 - Gestion bouton play
-//  3 - Gestion fenêtre modale
+//  2 - Gestion fenêtre modale
 
 const image_test = document.getElementById("image-test")
 
-//Permets de récupérer les id de toutes les sections de la page HTML
-const all_genres = document.getElementsByTagName("section")
-console.log(all_genres)
-for (var i = 0; i < all_genres.length; i++) {
-    console.log(all_genres[i].id)
-}
+const all_sections = document.getElementsByTagName("section")
+
+//Permets de récupérer les id de toutes les sections de la page HTML pour avoir le genre
+//function getGenreInHtml(all_sections) {
+//    let sections = []
+//    for (var i = 0; i < all_sections.length; i++) {
+//        let add_section_id = sections.push(all_sections[i].id)
+//    }
+//    let genres = []
+//    sections.forEach(function(item) {
+//        if (item.startsWith("best")) {
+//            console.log("rien")
+//        } else {
+//            let add_genre = genres.push(item)
+//        }
+//    })
+//    return genres
+//}
+
+const genre_list = ["animation", "comedy", "fantasy"]
 
 //Remplace la première image de la section "meilleurs films" par une image test depuis l'API
 fetch("http://localhost:8000/api/v1/titles/").then(function(res){
@@ -39,8 +56,6 @@ fetch("http://localhost:8000/api/v1/titles/").then(function(res){
 //        image_a_rendre.src=image_url
 //    })
 
-let num_incr = 0
-
 //for (movie in movies_by_category) {
 //    num_incr += 1
 //    let str_num = num_incr.toString()
@@ -48,38 +63,54 @@ let num_incr = 0
 ////    let genre =
 //}
 
+function replaceImages(genre, fetchedFilmIndex, new_image) {
+    let genres_url = BASE_URL + "?" + genre_filter + genre + "&" + SORT_BY_RATING
 
-
-function get_genre(url) {
-
-}
-
-function get_movies_by_category(api_url_by_categories) {
-    fetch(api_url_by_categories).then(function(res) {
+    fetch(genres_url).then(function(res) {
         return res.json()
+    }).then(function(data){
+        for (movie in data) {
+            const image_url = data["results"][fetchedFilmIndex]["image_url"]
+            new_image.src = image_url
+        }
     })
 }
 
-function get_movie_image(movie) {
-    image_url = movie["results"][i]["image_url"]
-    console.log(image_url)
-    new_image.src = image_url
+
+let html_images = document.querySelectorAll("#animation > .cat-row > .slider > img")
+console.log(html_images)
+
+function replace_images_in_categories(genre, html_images) {
+    html_images.forEach((image, index) => {
+        console.log(image)
+        const image_to_replace = image
+        replaceImages(genre, index, image_to_replace)
+    })
 }
 
-//function replace_images(api_url_by_categories, i, new_image) {
-//    fetch(api_url_by_categories).then(function(res) {
-//        return res.json()
-//    }).then(function(movie){
-//        image_url = movie["results"][i]["image_url"]
-//        console.log(image_url)
-//        new_image.src = image_url
-//    })
-//}
+//replace_images_in_categories(html_images)
 
-fetch("http://localhost:8000/api/v1/titles/?imdb_score=&genre=animation").then(function(res) {
-    return res.json()
-}).then(function(data){
-    for (movie in data) {
-
-    }
+//let genre_list = getGenreInHtml(all_sections)
+genre_list.forEach(function(genre) {
+//    let genre = genre_in_list
+    const html_images = document.querySelectorAll(`#${genre} > .cat-row > .slider > img`)
+    replace_images_in_categories(genre, html_images)
 })
+
+for (let pas = 0; pas < 5; pas++) {
+    let genre = genre_list[0]
+//    let html_image = document.querySelectorAll("img.slider")
+//    console.log(html_image)
+//    replace_images(genre, pas, )
+}
+
+//let animation_page = 1
+//let animation_films = []
+//const films = fetch...
+//animation_films.push(films)
+//bouton_suivant.addEventListener(click, function() {
+//    animation_page += 1
+//    next_films = fetch... en utilisant animation_page
+//    animation_films.push(next_films)
+//    replace_images() : à partir de animation_films et animation_page, savoir lesquels mettre
+//})
