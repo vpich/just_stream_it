f4db855ac8b08ec7b7a03ba10311c8a297dd5b25
Virginie PICH <red_iv@hotmail.fr>
2022-11-06 20:14:57 +0100

    Réorganisation de l'ordre du code js + fonction qui permet de récupérer les info du meilleur film automatiquement + modification de quelques id dans html

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 
 const BASE_URL = "http://localhost:8000/api/v1/titles/"
 const genre_filter = "genre="
-const rating_filter = "imdb_score="
 const SORT_BY_RATING = "sort_by=-imdb_score"
 const PAGE_FILTER = "page="
 
@@ -10,16 +9,42 @@ const PAGE_FILTER = "page="
 //  1 - Gestion slides
 //  2 - Gestion fenêtre modale
 
-const genre_list = ["animation", "comedy", "fantasy"]
+const genre_list = ["best_rated", "Animation", "Comedy", "Sci-Fi"]
 
-//for (movie in movies_by_category) {
-//    num_incr += 1
-//    let str_num = num_incr.toString()
-//    let new_image = "image-test" + str_num
-////    let genre =
-//}
+//
+//Gestion et affichage du meilleur film en haut de page
+//
 
-function replaceImages(genre, fetchedFilmIndex, new_image) {
+const best_movie_image = document.querySelector("#best_movie > img")
+const best_movie_title = document.querySelector("#best_movie > div > a")
+const best_movie_description = document.getElementById("description")
+
+function get_best_movie_info() {
+    const url = BASE_URL + "?" + SORT_BY_RATING
+
+    fetch(url).then(function(res) {
+        return res.json()
+    }).then(function(data){
+        best_movie_url = data["results"][0]["url"]
+        fetch(best_movie_url).then(function(res) {
+            return res.json()
+        }).then(function(data){
+            const db_title = data["title"]
+            const db_description = data["description"]
+            title.innerHTML = db_title
+            description.innerHTML = db_description
+        })
+    })
+}
+
+get_best_movie_info()
+replaceImage("", 0, best_movie_image)
+
+//
+// Remplacement des images dans chaque catégorie
+//
+
+function replaceImage(genre, fetchedFilmIndex, new_image) {
 
     let page = ""
 
@@ -43,24 +68,68 @@ function replaceImages(genre, fetchedFilmIndex, new_image) {
 
 function replace_images_in_categories(genre, html_images) {
     html_images.forEach((image, index) => {
-        replaceImages(genre, index, image)
+        replaceImage(genre, index, image)
     })
 }
 
 genre_list.forEach(function(genre) {
     const html_images = document.querySelectorAll(`#${genre} > .cat-row > .slider > img`)
-    replace_images_in_categories(genre, html_images)
+    if (genre == "best_rated") {
+        replace_images_in_categories("", html_images)
+    } else {
+        replace_images_in_categories(genre, html_images)
+    }
 })
 
+//
+//Slide des meilleurs films
+//
 const best_movies = document.querySelectorAll(`#best_rated > .cat-row > .slider > img`)
-replace_images_in_categories("", best_movies)
 
-const best_movies_button = document.querySelector("#best_rated > .cat-row > button.next")
+let number_click = 0
 
-best_movies_button.addEventListener("click", function() {
-    best_movies.forEach((image, index) => {
-        replaceImages("", index+=7, image)
+function previous_movies_selection(genre, genre_str) {
+    if (number_click > 0) {
+        number_click -= 1
+        genre.forEach((image, index) => {
+            replaceImage(genre_str, index+=(7 * number_click), image)
+        })
+    } else {
+    console.log("On est au début")
+    }
+}
+
+function next_movies_selection(genre, genre_str) {
+    number_click += 1
+    genre.forEach((image, index) => {
+        replaceImage(genre_str, index+=(7 * number_click), image)
     })
+    console.log(number_click)
+}
+
+const best_movies_button_previous = document.querySelector("#best_rated > .cat-row > button.previous")
+best_movies_button_previous.addEventListener("click", function() {
+    previous_movies_selection(best_movies, "")
+})
+
+const best_movies_button_next = document.querySelector("#best_rated > .cat-row > button.next")
+best_movies_button_next.addEventListener("click", function() {
+    next_movies_selection(best_movies, "")
+})
+
+//
+//Slide des films d'animation
+//
+const best_animation = document.querySelectorAll("#Animation > .cat-row > .slider > img")
+
+const best_animation_button_previous = document.querySelector("#Animation > .cat-row > button.previous")
+best_animation_button_previous.addEventListener("click", function() {
+    previous_movies_selection(best_animation, "Animation")
+})
+
+const best_animation_button_next = document.querySelector("#Animation > .cat-row > button.next")
+best_animation_button_next.addEventListener("click", function() {
+    next_movies_selection(best_animation, "Animation")
 })
 
 //let animation_page = 1
